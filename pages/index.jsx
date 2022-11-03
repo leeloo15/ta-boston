dbca3f82a0e385e66d2f68e14e6882287a588849
Leeloo <leila150788@gmail.com>
2022-11-03 19:58:35 +0200

    19:58

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,8 @@
+import Image from "next/image";
 
 export default function Home() {
-  return <div>hello boston <i className="fa-solid fa-house"></i></div>;
+  return (
+    <div>hello boston <i className="fa-solid fa-house"></i>
+    <img src="/images/logo.jpg" alt="Alt" /></div>
+  );
 }

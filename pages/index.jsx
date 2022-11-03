b4674bef4e10c17a4fb88308fc24e4f8c91851ab
Leeloo <leila150788@gmail.com>
2022-11-03 22:02:42 +0200

    TA din curs

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,6 +3,13 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>hello boston <i className="fa-solid fa-house"></i>
-    <img src="/images/logo.jpg" alt="Alt" /></div>
+    <img src="/images/logo.jpg" alt="Alt" />
+    <button className="button button-primary">Button Primary</button>
+    <button className="button button-primary button-large">Button primary large</button>
+
+    <a className="button button-primary">Button Primary</a>
+    <a className="button button-primary button-large">Button primary large</a>
+
+    </div>
   );
 }
